72bba4ca2aeac5dd38372cee7a423d7971770e6f
Nathan van Beelen <nathan@vanbeelen.org>
2019-10-03 10:23:58 +0200

    Add new assignment

---
+++ Assignment 2/KMeans.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+public class KMeans extends ClusteringAlgorithm
+{
+	// Number of clusters
+	private int k;
+
+	// Dimensionality of the vectors
+	private int dim;
+	
+	// Threshold above which the corresponding html is prefetched
+	private double prefetchThreshold;
+	
+	// Array of k clusters, class cluster is used for easy bookkeeping
+	private Cluster[] clusters;
+	
+	// This class represents the clusters, it contains the prototype (the mean of all it's members)
+	// and memberlists with the ID's (which are Integer objects) of the datapoints that are member of that cluster.
+	// You also want to remember the previous members so you can check if the clusters are stable.
+	static class Cluster
+	{
+		float[] prototype;
+
+		Set<Integer> currentMembers;
+		Set<Integer> previousMembers;
+		  
+		public Cluster()
+		{
+			currentMembers = new HashSet<Integer>();
+			previousMembers = new HashSet<Integer>();
+		}
+	}
+	// These vectors contains the feature vectors you need; the feature vectors are float arrays.
+	// Remember that you have to cast them first, since vectors return objects.
+	private Vector<float[]> trainData;
+	private Vector<float[]> testData;
+
+	// Results of test()
+	private double hitrate;
+	private double accuracy;
+	
+	public KMeans(int k, Vector<float[]> trainData, Vector<float[]> testData, int dim)
+	{
+		this.k = k;
+		this.trainData = trainData;
+		this.testData = testData; 
+		this.dim = dim;
+		prefetchThreshold = 0.5;
+		
+		// Here k new cluster are initialized
+		clusters = new Cluster[k];
+		for (int ic = 0; ic < k; ic++)
+			clusters[ic] = new Cluster();
+	}
+
+
+	public boolean train()
+	{
+	 	//implement k-means algorithm here:
+		// Step 1: Select an initial random partioning with k clusters
+		// Step 2: Generate a new partition by assigning each datapoint to its closest cluster center
+		// Step 3: recalculate cluster centers
+		// Step 4: repeat until clustermembership stabilizes
+		return false;
+	}
+
+	public boolean test()
+	{
+		// iterate along all clients. Assumption: the same clients are in the same order as in the testData
+		// for each client find the cluster of which it is a member
+		// get the actual testData (the vector) of this client
+		// iterate along all dimensions
+		// and count prefetched htmls
+		// count number of hits
+		// count number of requests
+		// set the global variables hitrate and accuracy to their appropriate value
+		return true;
+	}
+
+
+	// The following members are called by RunClustering, in order to present information to the user
+	public void showTest()
+	{
+		System.out.println("Prefetch threshold=" + this.prefetchThreshold);
+		System.out.println("Hitrate: " + this.hitrate);
+		System.out.println("Accuracy: " + this.accuracy);
+		System.out.println("Hitrate+Accuracy=" + (this.hitrate + this.accuracy));
+	}
+	
+	public void showMembers()
+	{
+		for (int i = 0; i < k; i++)
+			System.out.println("\nMembers cluster["+i+"] :" + clusters[i].currentMembers);
+	}
+	
+	public void showPrototypes()
+	{
+		for (int ic = 0; ic < k; ic++) {
+			System.out.print("\nPrototype cluster["+ic+"] :");
+			
+			for (int ip = 0; ip < dim; ip++)
+				System.out.print(clusters[ic].prototype[ip] + " ");
+			
+			System.out.println();
+		 }
+	}
+
+	// With this function you can set the prefetch threshold.
+	public void setPrefetchThreshold(double prefetchThreshold)
+	{
+		this.prefetchThreshold = prefetchThreshold;
+	}
+}
---
+++ Assignment 2/Kohonen.java
@@ -0,0 +1,128 @@
+import java.util.*;
+
+public class Kohonen extends ClusteringAlgorithm
+{
+	// Size of clustersmap
+	private int n;
+
+	// Number of epochs
+	private int epochs;
+	
+	// Dimensionality of the vectors
+	private int dim;
+	
+	// Threshold above which the corresponding html is prefetched
+	private double prefetchThreshold;
+
+	private double initialLearningRate; 
+	
+	// This class represents the clusters, it contains the prototype (the mean of all it's members)
+	// and a memberlist with the ID's (Integer objects) of the datapoints that are member of that cluster.  
+	private Cluster[][] clusters;
+
+	// Vector which contains the train/test data
+	private Vector<float[]> trainData;
+	private Vector<float[]> testData;
+	
+	// Results of test()
+	private double hitrate;
+	private double accuracy;
+	
+	static class Cluster
+	{
+			float[] prototype;
+
+			Set<Integer> currentMembers;
+
+			public Cluster()
+			{
+				currentMembers = new HashSet<Integer>();
+			}
+	}
+	
+	public Kohonen(int n, int epochs, Vector<float[]> trainData, Vector<float[]> testData, int dim)
+	{
+		this.n = n;
+		this.epochs = epochs;
+		prefetchThreshold = 0.5;
+		initialLearningRate = 0.8;
+		this.trainData = trainData;
+		this.testData = testData; 
+		this.dim = dim;       
+		
+		Random rnd = new Random();
+
+		// Here n*n new cluster are initialized
+		clusters = new Cluster[n][n];
+		for (int i = 0; i < n; i++)  {
+			for (int i2 = 0; i2 < n; i2++) {
+				clusters[i][i2] = new Cluster();
+				clusters[i][i2].prototype = new float[dim];
+			}
+		}
+	}
+
+	
+	public boolean train()
+	{
+		// Step 1: initialize map with random vectors (A good place to do this, is in the initialisation of the clusters)
+		// Repeat 'epochs' times:
+			// Step 2: Calculate the squareSize and the learningRate, these decrease lineary with the number of epochs.
+			// Step 3: Every input vector is presented to the map (always in the same order)
+			// For each vector its Best Matching Unit is found, and :
+				// Step 4: All nodes within the neighbourhood of the BMU are changed, you don't have to use distance relative learning.
+		// Since training kohonen maps can take quite a while, presenting the user with a progress bar would be nice
+		return true;
+	}
+	
+	public boolean test()
+	{
+		// iterate along all clients
+		// for each client find the cluster of which it is a member
+		// get the actual testData (the vector) of this client
+		// iterate along all dimensions
+		// and count prefetched htmls
+		// count number of hits
+		// count number of requests
+		// set the global variables hitrate and accuracy to their appropriate value
+		return true;
+	}
+
+
+	public void showTest()
+	{
+		System.out.println("Initial learning Rate=" + initialLearningRate);
+		System.out.println("Prefetch threshold=" + prefetchThreshold);
+		System.out.println("Hitrate: " + hitrate);
+		System.out.println("Accuracy: " + accuracy);
+		System.out.println("Hitrate+Accuracy=" + (hitrate + accuracy));
+	}
+ 
+ 
+	public void showMembers()
+	{
+		for (int i = 0; i < n; i++)
+			for (int i2 = 0; i2 < n; i2++)
+				System.out.println("\nMembers cluster["+i+"]["+i2+"] :" + clusters[i][i2].currentMembers);
+	}
+
+	public void showPrototypes()
+	{
+		for (int i = 0; i < n; i++) {
+			for (int i2 = 0; i2 < n; i2++) {
+				System.out.print("\nPrototype cluster["+i+"]["+i2+"] :");
+				
+				for (int i3 = 0; i3 < dim; i3++)
+					System.out.print(" " + clusters[i][i2].prototype[i3]);
+				
+				System.out.println();
+			}
+		}
+	}
+
+	public void setPrefetchThreshold(double prefetchThreshold)
+	{
+		this.prefetchThreshold = prefetchThreshold;
+	}
+}
+
